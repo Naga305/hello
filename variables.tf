243f3b0fb9a368fe97555adcbd0fe6794ca24c36
Naga305 <94951792+Naga305@users.noreply.github.com>
2022-01-09 21:59:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "MyNewSecurityGroup"
+  default     = "defaultsecuritygroup"
 }
 
 variable "tag_name" {
