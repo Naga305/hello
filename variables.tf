042c50fe8e291b2b80111c246546163dd321deb7
Naga305 <94951792+Naga305@users.noreply.github.com>
2022-01-09 22:08:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "terraformsecuritygroup"
+  default     = "MyNewSecurityGroup"
 }
 
 variable "tag_name" {
