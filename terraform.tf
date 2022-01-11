1070f05937879218360cdf3970c6b1d9627c6288
Naga305 <94951792+Naga305@users.noreply.github.com>
2022-01-11 07:35:29 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -13,6 +13,6 @@ provider "aws" {
    } 
 }
 resource "aws_key_pair" "key" {
-    key_name = "Jenkins-Keypair"
+    key_name = "new-key"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDnelel2vbkig9goQ2+Jha6rpOSxyB6keeGZ8kKnCvMN4i36JX8HSTaR8gajDQfwdX6gCi/JtseusP5kDW70khTJ2GqdKzBiSwFSbSfDn0/8FVxNubp7ObGO8vquPQNTybracAyQkrxwlt91NzFYN4GJbbrIU5fX807hy284Pl5o1SCfkrNYE4EfF4dp4MDQCe1nVnqTwUGhgwQCHciRyl+X6RnuULLcJ5ThM4lJ8ET+TRc7TANKPnzWkJhUCvEWnWj0jkRsf0m28iMSisArHMqUT6x0b7tbbtadKCH+mf6KDiiXCB2XcLDUaJrkhgrEfIEgENqf+QggpYcYRyj/yfsG7JQthcuEdNcNf/Q+MdPXM+N5cUIuVd7aaf1oEkz8/PmE14yx+J+E2KloA6QcxcrCBv60MfgWB24S/cTYsLf1Hotle/zmqjyFHlcR+NzrRqrjaTIRLDv77k4/a35LSYM/AwGedNEPuVeRdInlogur7buCrUgszps96b3QYBCB0k= naga abishek@LAPTOP-T9HUNH5H"
 } 
